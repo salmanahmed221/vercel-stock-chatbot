e794f6ffa29faeeb83514b0b92d7aeb582a42f01
salmanahmed221 <salmanahmed121221@gmail.com>
2024-03-29 18:58:47 +0500

    text justify

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -24,7 +24,7 @@ export function EmptyScreen() {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">Welcome to Stock AI Chatbot!</h1>
-        <p className="leading-normal text-muted-foreground">
+        <p className="leading-normal text-muted-foreground text-justify">
           Introducing StockAI Chatbot, your intelligent companion in the world
           of stock trading and investment. StockAI is a cutting-edge chatbot
           powered by advanced artificial intelligence algorithms, designed to
